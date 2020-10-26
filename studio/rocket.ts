ddf4c1db3f3120111f67cd654c11594600af9712
Rhiannon Davies <ro.morningstar@gmail.com>
2020-10-26 10:46:03 -0500

    completed studio

---
+++ studio/rocket.ts
@@ -0,0 +1,45 @@
+import {Payload} from './Payload';
+import {Cargo} from './cargo';
+import {Astronaut} from './astronaut';
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    sumMass (items: Payload[]): number {
+        let totalMass = 0;
+        for (let i = 0; i < items.length; i++) {
+            totalMass += items[i].massKg;
+        }
+        return totalMass;
+    }
+    currentMassKg(): number {
+        return (this.sumMass(this.astronauts) + this.sumMass(this.cargoItems));
+    }
+    canAdd(item: Payload): boolean {
+        if ((this.currentMassKg() + item.massKg) <= this.totalCapacityKg) {
+            return true;
+        }
+    }
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut) === true) {
+            this.astronauts.push(astronaut);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
